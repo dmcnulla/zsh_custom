24d45f8e83f91e1b1d604ad5072b8b3ef6e1f413
Dave McNulla <mcnulla@gmail.com>
2015-07-16 21:36:59 -0700

    local custom zsh

---
+++ dave.zsh
@@ -0,0 +1,32 @@
+alias e='chmod +x '
+
+#sublime
+alias s='sublime '
+
+#tar
+alias tj='tar -xvjpf'
+alias t='tar xvfz '
+alias tz='tar -xvzpf'
+
+#sudo stuff
+alias sapt='sudo apt-get '
+alias supd='sudo apt-get update '
+alias sup='sudo apt-get upgrade '
+alias sin='sudo apt-get install '
+alias srem='sudo apt-get remove '
+alias repos='sudo add-apt-repository '
+alias _='sudo '
+
+# alias jenkins='nohup java -jar /devroot/tools/jenkins/jenkins.war --httpPort=5050 > jenkins.log 2>&1'
+# alias redmine='ruby script/rails server webrick -e production &'
+# alias elastic='cd /devroot/tools/elasticsearch-0.19.12 && bin/elasticsearch -f &'
+# alias grayserver='cd /devroot/tools/graylog2-server-0.9.6p1/bin  && ./graylog2ctl start' 
+# alias grayweb='cd /devroot/tools/graylog2-web-interface-0.9.6p1/script && rails s -p 3002 &'
+
+#uShare
+alias uMovies='ushare -c /home/dave/Videos/Movies'
+alias uVideo='ushare -c /home/dave/Videos'
+alias uPictures='ushare -c /home/dave/Pictures'
+alias uMusic='ushare -c /home/dave/Music'
+
+
