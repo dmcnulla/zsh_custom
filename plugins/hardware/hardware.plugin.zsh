83867df3c9e73d6217212868b416b6d272c32881
Dave McNulla <mcnulla@gmail.com>
2015-07-16 21:38:46 -0700

    removed unused files & directories

--- plugins/hardware/hardware.plugin.zsh
+++
@@ -1,9 +0,0 @@
-getID() {
-	xinput list|grep "Synaptics TouchPad"|awk '{print $6 }'|cut -d'=' -f2
-}
-
-alias laptopprimary='xrandr --output LVDS --primary'
-
-alias tpf='xinput set-prop `getID` "Device Enabled" 0'
-alias tpn='xinput set-prop `getID` "Device Enabled" 1'
-
