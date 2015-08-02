afdaf63599b12976d558ca92f41612f71ffbf842
Dave McNulla <mcnulla@gmail.com>
2015-08-02 05:17:20 -0700

    Custom zsh files.

---
+++ dave.zsh
@@ -0,0 +1,18 @@
+alias e='chmod +x '
+
+#sublime
+alias s='subl '
+
+#tar
+alias tj='tar -xvjpf'
+alias t='tar xvfz '
+alias tz='tar -xvzpf'
+
+#sudo stuff
+alias sapt='sudo apt-get '
+alias supd='sudo apt-get update '
+alias sup='sudo apt-get upgrade '
+alias sin='sudo apt-get install '
+alias srem='sudo apt-get remove '
+alias repos='sudo add-apt-repository '
+alias _='sudo '
---
+++ network.zsh
@@ -0,0 +1,1 @@
+alias inet='ifconfig | grep inet'
