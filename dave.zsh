bf964346d2a0fc24cae04e78a62ff9ec627acf7a
Dave McNulla <mcnulla@gmail.com>
2015-08-02 06:28:42 -0700

    removed unused aliases

--- dave.zsh
+++ dave.zsh
@@ -17,16 +17,3 @@ alias srem='sudo apt-get remove '
 alias repos='sudo add-apt-repository '
 alias _='sudo '
 
-# alias jenkins='nohup java -jar /devroot/tools/jenkins/jenkins.war --httpPort=5050 > jenkins.log 2>&1'
-# alias redmine='ruby script/rails server webrick -e production &'
-# alias elastic='cd /devroot/tools/elasticsearch-0.19.12 && bin/elasticsearch -f &'
-# alias grayserver='cd /devroot/tools/graylog2-server-0.9.6p1/bin  && ./graylog2ctl start' 
-# alias grayweb='cd /devroot/tools/graylog2-web-interface-0.9.6p1/script && rails s -p 3002 &'
-
-#uShare
-alias uMovies='ushare -c /home/dave/Videos/Movies'
-alias uVideo='ushare -c /home/dave/Videos'
-alias uPictures='ushare -c /home/dave/Pictures'
-alias uMusic='ushare -c /home/dave/Music'
-
-
