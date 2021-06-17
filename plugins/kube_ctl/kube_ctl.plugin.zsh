a58d1118d01f19d84628432a65f36036b02d1ef7
David McNulla <david.mcnulla@teradata.com>
2021-06-16 17:06:23 -0700

    fix namespace calls

--- plugins/kube_ctl/kube_ctl.plugin.zsh
+++ plugins/kube_ctl/kube_ctl.plugin.zsh
@@ -1,28 +1,26 @@
-export NAMESPACE='--namespace jenkins'
-
 alias delete_pods="find_pods | awk '{print \$1;}' | xargs del_pod"
 
 find_pods() {
   echo "find_pods"
-  kubectl $NAMESPACE get pods | grep "Error\|ImagePullBackOff\|ErrImagePull"
+  kubectl --namespace jenkins get pods | grep "Error\|ImagePullBackOff\|ErrImagePull"
 }
 
 del_pod() {
   echo "del_pod <pod_name>"
-  kubectl $NAMESPACE --grace-period=0 delete pod $1
+  kubectl --namespace jenkins --grace-period=0 delete pod $1
 }
 
 ssh_kube() {
   echo "ssh_kube <pod_name>"
-  kubectl $NAMESPACE exec -c sit-tests -it $1 bash
+  kubectl --namespace jenkins exec -c sit-tests -it $1 bash
 }
 
 port_forward_kube() {
   echo "port_fortward_kube <service_name> <port_to_forward>"
-  kubectl port-forward $NAMESPACE svc/$1 $2:$2
+  kubectl port-forward --namespace jenkins svc/$1 $2:$2
 }
 
 cp_kube() {
   echo "cp_kube <pod_name> <path_to_file>"
-  kubectl $NAMESPACE $1:$2 $2:t
+  kubectl --namespace jenkins $1:$2 $2:t
 }
